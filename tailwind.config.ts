b50093c4ca7ccd4be4bf17df10d01e0eea8159e6
Kawser Simanto <kawsersimanto@gmail.com>
2024-12-07 16:22:49 +0600

    initialized fonts and page title

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
